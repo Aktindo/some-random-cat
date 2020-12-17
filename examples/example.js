4577af3a9c5dd1cf7d126c05016bd287f7bc4567
Aktindo <71206933+Aktindo@users.noreply.github.com>
2020-12-17 13:28:41 +0530

    Update example.js

--- examples/example.js
+++ examples/example.js
@@ -1,12 +1,5 @@
-// This snippet uses some-random-cat version 2.0
+// This snippet uses some-random-cat version 2.0+
 
-const somethingRandom = require('../index').Random // Replace ../index with the pacakge name: some-random-cat
-somethingRandom.getDog()
-.then(res => {
-    console.log(res)
-    return
-}) // Get the result
-.catch(e => {
-    console.error(e)
-    return
-}) // Catch the error
+const somethingRandom = require('../index').Generation
+const res = somethingRandom.formatTime(Date.now())
+console.log(res)
